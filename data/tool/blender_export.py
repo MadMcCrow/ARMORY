69644395d75c950b34912c3bec3791c7b592dd0c
MadMcCrow <perard.git@outlook.com>
2021-01-07 13:08:48 +0100

    :art: :construction: changed tile meshes, added icon svg

--- data/tool/blender_export.py
+++ data/tool/blender_export.py
@@ -9,16 +9,30 @@
 
 import bpy
 from os import path
+from os import makedirs
+
+def check_folder_export(exportFolder : str) ->str :
+    folder = path.realpath(path.join( bpy.path.abspath("//"), exportFolder))
+    try:
+        print(folder)
+        makedirs(folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    finally:
+        return folder
 
 def export_all_obj(exportFolder):
     ''' 
     xport every object in scene to obj
     '''
+    folder = check_folder_export(exportFolder)
     objects = bpy.data.objects
     for object in objects:
         bpy.ops.object.select_all(action='DESELECT')
         object.select_set(state=True)
-        exportName = path.join(exportFolder, object.name + '.obj')
+        print("exporting {} to obj".format(object.name))
+        exportName = path.join(folder, object.name + '.obj')
         bpy.ops.export_scene.obj(   filepath=exportName, check_existing=False,
                                     axis_forward='-Z', axis_up='Y',
                                     filter_glob="*.obj;*.mtl",
