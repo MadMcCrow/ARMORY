cd9b4e35ebd7200db80e8562ea37bddf3c759c8e
MadMcCrow <perard.git@outlook.com>
2021-09-26 18:06:04 +0200

    end of c++ dev

--- build/build.py
+++ build/build.py
@@ -4,7 +4,7 @@
 #               functions dedicated to building this project
 #				this replaces the improper makefile that used to call scons
 #
-# Copyright © Noé Perard-Gayot 2020.                                                                                        #
+# Copyright © Noé Perard-Gayot 2021.                                                                                        #
 # Licensed under the MIT License. You may obtain a copy of the License at https://opensource.org/licenses/mit-license.php   #
 #
 
