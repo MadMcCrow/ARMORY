5c9e1740d561ab9e984fde77beccf759baf07c2b
MadMcCrow <noe.perard@live.ru>
2022-07-26 23:02:16 +0200

    :snowflake: start using flake.nix

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+    inputs = {
+    };
+    outputs = {self, nixpkgs, ...}@inputs: 
+        let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs{inherit system;};
+        in
+    {
+        packages."${system}" = with pkgs; {
+            default = stdenv.mkDerivation rec{
+                name = "godot";
+                src = inputs.godot;
+                nativeBuildInputs = [
+                    scons
+                    pkg-config
+                    xorg.libX11
+                    xorg.libXcursor
+                    xorg.libXinerama
+                    xorg.libXrandr
+                    xorg.libXrender
+                    xorg.libXi
+                    xorg.libXext
+                    xorg.libXfixes
+                    udev
+                    systemd
+                    systemd.dev
+                    libpulseaudio
+                    freetype
+                    openssl
+                    alsa-lib
+                    libGLU
+                    zlib
+                    yasm
+
+                ];
+                patchPhase = ''
+                    substituteInPlace platform/linuxbsd/detect.py --replace 'pkg-config xi ' 'pkg-config xi xfixes '
+                '';
+                enableParallelBuilding = true;
+                buildInputs = nativeBuildInputs;
+                
+                sconsFlags = "platform=linuxbsd";
+                LIBRARY_PATH = "${xorg.libXfixes}";
+                installPhase = ''
+                    mkdir -p "$out/bin"
+                    cp bin/godot.* $out/bin/godot
+                '';
+            };
+        };
+
+    };
+}
+
