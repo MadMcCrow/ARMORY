4da655626be5a24de5f132a32038efae653c907a
Noé Perard-Gayot <noe.perard-gayot@dont-nod.com>
2023-06-20 17:29:10 +0200

    win64 cleanup

--- win64/build.bat
+++ win64/build.bat
@@ -5,9 +5,10 @@
 ::
 :: Copyright © Noé Perard-Gayot 2023.
 :: Licensed under the MIT License. You may obtain a copy of the License at https://opensource.org/licenses/mit-license.php
-
+@CD src
 @RD /S /Q godot-cpp
-git clone https://github.com/godotengine/godot-cpp --depth 1 -q
+@git clone https://github.com/godotengine/godot-cpp --depth 1 -q
 @DEL /Q "godot-cpp/.git"
 @CD armory
-scons
+@scons
+@CP bin/* ../armory/bin/win64
